f3f88e67b7457a04b147662568f1efe67f43458c
bhaskar-9624 <104679492+bhaskar-9624@users.noreply.github.com>
2026-01-28 11:46:38 +0530

    Update network-policy-minikube.sh

--- network-policy-minikube.sh
+++ network-policy-minikube.sh
@@ -18,3 +18,23 @@ sudo usermod -aG docker $USER && newgrp docker
 
 minikube start --cni=calico
 minikube status 
+
+
+
+Extra  my ref:
+
+#Kubectl Install
+#export PATH=$PATH:/usr/local/bin
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+Minikubeinstall:
+
+
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x minikube-linux-amd64 
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+minikube version
